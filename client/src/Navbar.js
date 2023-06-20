a9adeb3a9d55a3b1ef7dc9e9de013895f97223af
Abidijihed <abidij55@gmail.com>
2023-06-20 14:14:11 +0100

    save

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,76 +1,245 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, InputBase, IconButton, Menu, MenuItem } from '@mui/material';
-import { ShoppingCart, Search, Menu as MenuIcon } from '@mui/icons-material';
-import './Navbar.css';
+import React, { useEffect, useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Badge,
+  InputBase,
+  Hidden,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  Avatar,
+} from '@mui/material';
+import {
+  makeStyles,
+} from '@material-ui/core';
+import {alpha } from '@mui/material/styles';
+import {
+  FaShoppingCart,
+  FaUser,
+  FaPhoneAlt,
+  FaInfo,
+  FaSignInAlt,
+  FaSearch,
+  FaBars,
+ 
+} from 'react-icons/fa';
+import { 
+   MdOutlineMailOutline,
+  MdOutlineAddShoppingCart
+ } from 'react-icons/md';
 
-const Navbar = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+import { Link } from 'react-router-dom';
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: '#e8b623',
+    zIndex: theme.zIndex.drawer + 1,
+    display: 'flex',
+    justifyContent: 'space-between',
+    [theme.breakpoints.up('md')]: {
+      justifyContent: 'flex-start',
+    },
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchInput: {
+    marginLeft: theme.spacing(1),
+    color: 'black',
+    backgroundColor:"white"
+  },
+  searchIcon: {
+    marginLeft: theme.spacing(1),
+    color:"black"
+  },
+  navIcons: {
+    display: 'flex',
+    alignItems: 'initial',
+  },
+  navIcon: {
+    marginLeft: theme.spacing(2),
+    fontSize:"25px",
+    '&:hover': {
+      backgroundColor: "white",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+}));
+
+const Navbar = ({ handleChange,shop})=> {
+  const token = localStorage.getItem("token");
+  const [mobileOpen, setMobileOpen] = useState(false);
+ 
+   const classes = useStyles();
+
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <AppBar position="sticky" sx={{ backgroundColor: 'gold', color: 'maroon' }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 1 }}>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontSize: 20, fontWeight: 'bold' }}>
-          Harmony Store
+  const drawer = (
+    <div>
+      <div className={classes.toolbar}>
+        <Typography variant="h6" align="center" style={{marginTop:"10px",textDecoration:"underline"}} component={Link} to="/">
+          HarmonyStore
         </Typography>
-        <div sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center' }}>
-          <InputBase sx={{ backgroundColor: 'white', color: 'black', border: 'none', padding: 1, marginRight: 1, width: 250 }} placeholder="Search" />
-          <IconButton sx={{ backgroundColor: 'gold', color: 'maroon' }}>
-            <Search />
-          </IconButton>
-        </div>
-        <div sx={{ display: { xs: 'flex', sm: 'none' }, alignItems: 'center' }}>
-          <IconButton
-            sx={{ backgroundColor: 'gold', color: 'maroon', marginRight: 1 }}
-            onClick={handleClick}
+      </div>
+      <Divider />
+      <List>
+        <ListItem button component={Link} to="/contact">
+          <ListItemIcon>
+            <MdOutlineMailOutline />
+          </ListItemIcon>
+          <ListItemText primary="Contact Us" />
+        </ListItem>
+        <ListItem button component={Link} to="/about">
+          <ListItemIcon>
+            <FaInfo />
+          </ListItemIcon>
+          <ListItemText primary="About Us" />
+        </ListItem>
+       {!token&& <ListItem button component={Link} to="/login">
+          <ListItemIcon>
+            <FaSignInAlt />
+          </ListItemIcon>
+          <ListItemText primary="Login" />
+        </ListItem>}
+      { token&& <ListItem button component={Link} to="/profile">
+          <ListItemIcon>
+            <FaUser />
+          </ListItemIcon>
+          <ListItemText primary="Profile" />
+        </ListItem>}
+        <Divider />
+        <ListItem button component={Link} to="/products">
+          <ListItemIcon>
+            <MdOutlineAddShoppingCart />
+          </ListItemIcon>
+          <ListItemText primary="Product List" />
+        </ListItem>
+      </List>
+    </div>
+  );
+  
+  return (
+    <>
+      <AppBar position="sticky" className={classes.appBar} sx={{backgroundColor:'#e8b623'}}>
+        <Toolbar className='mynavbar'>
+        <Typography variant="h6" align="center" style={{marginTop:"10px",textDecoration:"underline"}} component={Link} to="/">
+          HarmonyStore
+        </Typography>
+          {/* <Hidden mdDown>
+            <Typography className={classes.title} variant="h6" noWrap>
+              ElectroZayn
+            </Typography> */}
+          {/* </Hidden> */}
+          <Hidden mdUp>
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerToggle}
+            >
+              <FaBars />
+            </IconButton>
+          </Hidden>
+          {/* <Hidden smDown> */}
+            <div className={classes.search} id='allsearch'>
+              <div className={classes.searchIcon}>
+                <FaSearch />
+              </div>
+              <InputBase
+              id='search'
+                placeholder="Search..."
+                onChange={(e) => handleChange(e)}
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                className={classes.searchInput}
+              />
+            </div>
+          {/* </Hidden> */}
+          <div className={classes.navIcons}>
+            <Hidden smDown>
+              <IconButton color="inherit" className={classes.navIcon} component={Link} to="/profile" >
+                Profile
+              </IconButton>
+            {!token&&  <IconButton color="inherit" className={classes.navIcon} component={Link} to="/login">
+                {/* <FaSignInAlt /> */}
+                Login
+              </IconButton>}
+              <IconButton color="inherit" className={classes.navIcon} component={Link} to="/contact">
+                {/* <FaPhoneAlt /> */}
+                Contact Us
+              </IconButton>
+              <IconButton color="inherit" className={classes.navIcon} component={Link} to="/about">
+                {/* <FaInfo /> */}
+                About Us
+              </IconButton>
+              <IconButton color="inherit" className={classes.navIcon} component={Link} to="/products">
+                {/* <MdOutlineAddShoppingCart /> */}
+                Products
+              </IconButton>
+            </Hidden>
+            <IconButton color="inherit">
+              <Badge badgeContent={Number(shop)} color="secondary" component={Link} to="/chekout">
+                <FaShoppingCart fontSize="xlarge" color='white'/>
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Hidden mdUp>
+        <nav className={classes.drawer}>
+          <Drawer
+            variant="temporary"
+            anchor="left"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-            onClick={handleClose}
-            PaperProps={{
-              elevation: 0,
-              sx: {
-                backgroundColor: 'gold',
-                minWidth: 120,
-                boxShadow: (theme) => theme.shadows[8],
-                left: 0, // Position the dropdown on the left side of the input
-              },
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left', // Adjust the horizontal alignment of the dropdown
-            }}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-          >
-            <MenuItem>
-              <a href="#">Contact Us</a>
-            </MenuItem>
-            <MenuItem>
-              <a href="#">About Us</a>
-            </MenuItem>
-          </Menu>
-        </div>
-        <div sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton sx={{ backgroundColor: 'gold', color: 'maroon', marginLeft: 1 }}>
-            <ShoppingCart />
-          </IconButton>
-        </div>
-      </Toolbar>
-    </AppBar>
+            {drawer}
+          </Drawer>
+        </nav>
+      </Hidden>
+    </>
   );
 };
 
