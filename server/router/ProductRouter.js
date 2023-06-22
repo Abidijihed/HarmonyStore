cc7e0f05f49dfa44f422b2b297263e460de560fc
Abidijihed <abidij55@gmail.com>
2023-06-22 11:34:18 +0100

    save

--- server/router/ProductRouter.js
+++ server/router/ProductRouter.js
@@ -1,5 +1,5 @@
 const router=require("express").Router()
 const ProductController=require("../controllers/products")
  router.post('/api/product',ProductController.CreateProduct)
-
+// hh
  module.exports={ProductRouter:router}
